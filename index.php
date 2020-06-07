42fdef832c3a39fd2378c7683a976b66e3540c57
Net Learn <any23@dsjsc.com>
2020-06-07 12:07:57 -0500

    jeff added

--- index.php
+++ index.php
@@ -1,51 +1,53 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>Page Title</title>
-		<title>Bootstrap Example</title>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
   <style>
-
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
-    
- 
-
+  body {
+    position: relative; 
+  }
   </style>
-	</head>
-	<body>
-		<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-     
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="life.html">Discrepancy</a></li>
-        <li><a href="https://jefflikesto.wordpress.com">About</a></li>
-        <li><a href="button.html">Buttons</a></li>
-        <li><a href="jeff.html">Hope and despair</a></li>
-        <li><a href="test.html">Test</a></li>
-<li><a href="havenot.html">What do I have</a></li>
-<li><a href="simple.html">Simple really</a></li>
-      </ul>
-   
-    </div>
-  </div>
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="100">
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">  
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="https://jefflikesto.wordpress.co">About</a>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="https://jefflikesto.wordpress.com" id="navbardrop" data-toggle="dropdown">
+        Fun
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="test.html">Test</a>
+        <a class="dropdown-item" href="havenot.htm">What do I have</a>
+         <a class="dropdown-item" href="simple.html">Simple reall</a>
+      </div>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        More Fun
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="life.htm">Discrepancy</a>
+        <a class="dropdown-item" href="button.html">Buttons</a>
+         <a class="dropdown-item" href="jeff.html">Hope and despair</a>
+      </div>
+    </li>
+  </ul>
 </nav>
 
+ 
 
-  
-	</body>
+
+
+
+</body>
 </html>
